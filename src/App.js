7e5ba713467628a66abfa3dfb249c7c3b0c38691
kod0751 <50727457+kod0751@users.noreply.github.com>
2020-12-22 20:41:14 +0900

    Revert "에러 수정"
    This reverts commit 6089ae1828a69cbfad7b15d9c78011c36ea203e3.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import PostListContainer from './containers/PostListContainer';
+import CounterContainer from "./containers/CounterContainer";
 
 
 function App() {
-  return <PostListContainer />;
+  return (
+    <CounterContainer/>
+  );
 }
 
 export default App;
--- src/lib/asyncUtils.js
+++ src/lib/asyncUtils.js
@@ -1,7 +1,7 @@
 export const createPromiseThunk = (type, promiseCreator) => {
     const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
 
-    return param => async dispatch => {
+    const thunkCreator = param => async dispatch => {
         dispatch({ type })
         try {
             const payload = await promiseCreator(param);
