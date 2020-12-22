6089ae1828a69cbfad7b15d9c78011c36ea203e3
kod0751 <50727457+kod0751@users.noreply.github.com>
2020-12-22 20:40:26 +0900

    에러 수정

--- src/lib/asyncUtils.js
+++ src/lib/asyncUtils.js
@@ -1,7 +1,7 @@
 export const createPromiseThunk = (type, promiseCreator) => {
     const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
 
-    const thunkCreator = param => async dispatch => {
+    return param => async dispatch => {
         dispatch({ type })
         try {
             const payload = await promiseCreator(param);
