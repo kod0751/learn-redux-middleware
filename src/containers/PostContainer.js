f6cbd2fe6ea2823f3aadd58d053920ad882cd03d
kod0751 <50727457+kod0751@users.noreply.github.com>
2020-12-25 22:07:43 +0900

    redux-thunk로 Promise다루기6
    라우터연동, 특정 포스트읽기

---
+++ src/containers/PostContainer.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Post from '../components/Post';
+import { getPostById } from '../modules/posts';
+
+function PostContainer({ postId }) {
+    const { data, loading, error } = useSelector(state => state.posts.postById);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPostById(postId));
+    }, [postId, dispatch])
+
+    if (loading) return <div>로딩중...</div>
+    if (error) return <div>에러 발생!</div>
+    if (!data) return null;
+
+  return(
+    <Post post={data}/>
+  );
+}
+
+export default PostContainer;
