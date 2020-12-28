ec5d05e2be741e0faedff26106357d560380f2a8
kod0751 <50727457+kod0751@users.noreply.github.com>
2020-12-29 01:14:57 +0900

    Thunk함수에서 리액트 라우터 History 사용하기

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -13,6 +13,9 @@ const CLEAR_POST = 'CLEAR_POST';
 
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
 export const getPostById = createPromiseThunkById(GET_POSTBYID, postsAPI.getPostById);
+export const goToHome = () => (dispatch, getState, { history }) => {
+    history.push('/');
+}
 
 export const clearPost = () => ({ type: CLEAR_POST });
 
