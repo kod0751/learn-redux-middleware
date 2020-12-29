100df963cf0a3880c0f575f927ece9836c9a87f8
kod0751 <50727457+kod0751@users.noreply.github.com>
2020-12-29 23:58:20 +0900

    json서버

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,30 +1,11 @@
-const sleep =  n => new Promise(resolve => setTimeout(resolve, n));
-
-// { id, title, body }
-const posts = [
-    {
-        id: 1,
-        title: '리덕스 미들웨어',
-        body: '리덕스 미들웨어 만들기'
-    }, 
-    {
-        id: 2,
-        title: 'redux-thunk',
-        body: 'redux-thunk 만들기'
-    }, 
-    {
-        id: 3,
-        title: 'redux-saga',
-        body: 'redux-saga 만들기'
-    }, 
-]
+import axios from 'axios';
 
 export const getPosts = async () => {
-    await sleep(500);
-    return posts;
+    const response = await  axios.get('http://localhost:4000/posts');
+    return response.data;
 }
 
 export const getPostById = async id => {
-    await sleep(500);
-    return posts.find(post => post.id === id);
+    const response = await axios.get(`http://localhost:4000/posts/${id}`);
+    return response.data;
 }
