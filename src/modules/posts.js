b17e4bc7632b0c7194d38e892917f4e1f82e1fd8
kod0751 <50727457+kod0751@users.noreply.github.com>
2021-01-04 23:00:08 +0900

    redux-saga Promise 다루기

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,5 +1,6 @@
 import * as postsAPI from '../api/posts';
 import { createPromiseThunk, createPromiseThunkById, handleAsyncActions, handleAsyncActionsById, reducerUtils } from '../lib/asyncUtils';
+import { call, put, takeEvery } from 'redux-saga/effects'
 
 const GET_POSTS = 'GET_POSTS';
 const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
@@ -11,8 +12,56 @@ const GET_POSTBYID_ERROR = 'GET_POSTBYID_ERROR';
 
 const CLEAR_POST = 'CLEAR_POST';
 
-export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPostById = createPromiseThunkById(GET_POSTBYID, postsAPI.getPostById);
+// export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
+// export const getPostById = createPromiseThunkById(GET_POSTBYID, postsAPI.getPostById);  redux-thunk
+
+export const getPosts = () => ({ type: GET_POSTS });
+export const getPostById = id => ({ 
+    type: GET_POSTBYID, 
+    payload: id,
+    meta: id,
+});  //redux-saga
+
+function* getPostsSaga() {
+    try {
+        const posts = yield call(postsAPI.getPosts);
+        yield put({
+            type: GET_POSTS_SUCCESS,
+            payload: posts,
+        });
+    } catch (e) {
+        yield put({
+            type: GET_POSTS_ERROR,
+            payload: e,
+            error: true
+        });
+    }
+}
+
+function* getPostByIdSaga(action) {
+    const id = action.payload;
+    try {
+        const postById = yield call(postsAPI.getPostById, id);
+        yield put({
+            type: GET_POSTBYID_SUCCESS,
+            payload: postById,
+            meta: id
+        });
+    } catch (e) {
+        yield put({
+            type: GET_POSTBYID_ERROR,
+            payload: e,
+            error: true,
+            meta: id
+        });
+    }
+};
+
+export function* postsSaga() {
+    yield takeEvery(GET_POSTS, getPostsSaga);
+    yield takeEvery(GET_POSTBYID, getPostByIdSaga);
+}
+
 export const goToHome = () => (dispatch, getState, { history }) => {
     history.push('/');
 }
