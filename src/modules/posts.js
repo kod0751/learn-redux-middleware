656787a769a55fb3d3c76065b906154e0458a46a
kod0751 <50727457+kod0751@users.noreply.github.com>
2021-01-07 22:05:50 +0900

    redux-saga select
    현재 상태에 따라 조건부로 작업해야하는 경우에 사용

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,6 +1,6 @@
 import * as postsAPI from '../api/posts';
 import { createPromiseSaga, createPromiseSagaById, handleAsyncActions, handleAsyncActionsById, reducerUtils } from '../lib/asyncUtils'; // createPromiseThunk, createPromiseThunkById redux-thunk
-import { takeEvery, getContext } from 'redux-saga/effects'
+import { takeEvery, getContext, select } from 'redux-saga/effects'
 
 const GET_POSTS = 'GET_POSTS';
 const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
@@ -12,6 +12,7 @@ const GET_POSTBYID_ERROR = 'GET_POSTBYID_ERROR';
 const GO_TO_HOME = 'GO_TO_HOME';
 
 const CLEAR_POST = 'CLEAR_POST';
+const PRINT_STATE = 'PRINT_STATE';
 
 // export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
 // export const getPostById = createPromiseThunkById(GET_POSTBYID, postsAPI.getPostById);  redux-thunk
@@ -22,6 +23,7 @@ export const getPostById = id => ({
     payload: id,
     meta: id,
 });  //redux-saga
+export const printState = () => ({ type: PRINT_STATE });
 
 const getPostsSaga = createPromiseSaga(GET_POSTS, postsAPI.getPosts);
 const getPostByIdSaga = createPromiseSagaById(GET_POSTBYID, postsAPI.getPostById);
@@ -30,10 +32,16 @@ function* goToHomeSaga() {
     history.push('/');
 }
 
+function* printStateSaga() {
+    const state = yield select(state => state.posts);
+    console.log(state);
+}
+
 export function* postsSaga() {
     yield takeEvery(GET_POSTS, getPostsSaga);
     yield takeEvery(GET_POSTBYID, getPostByIdSaga);
     yield takeEvery(GO_TO_HOME, goToHomeSaga);
+    yield takeEvery(PRINT_STATE, printStateSaga);
 }
 
 export const goToHome = () => ({ type: GO_TO_HOME });
